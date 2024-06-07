3e205dbc29d683463969e77bcf8938634a6ab642
Vitor Antonionne <vitor-antonionne@hotmail.com>
2024-06-07 13:44:59 -0300

    att favicon, footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,23 +7,25 @@ import styles from '../style/Footer.module.css'
 export function Footer() {
   return(
     <div className={styles.footerContainer}>
-      <a href="#"><img src={logoTipo} alt="Logotipo osmar contabilidade" /></a>
-      <div className={styles.footerContactContainer}>
-        <h3>Dados de Contato</h3>
-        <ul>
-          <li><span><MapPin size={26} color='#FFEF37'/></span>R. Barão de aracati</li>
-          <li><span><Phone size={26}color='#FFEF37'/></span>(99)-9999999</li>
-          <li><span><Envelope size={26}color='#FFEF37'/></span>e-mail</li>
-        </ul>
-      </div>
 
-      <div className={styles.footerContactCalendary}>
+      <ul className={styles.footerImg}>
+        <a href="#"><img src={logoTipo} alt="Logotipo osmar contabilidade" /></a>
+        <p>CNPJ: 05.789.851/0001-89</p>
+      </ul>
+
+      <ul className={styles.footerContactContainer}>
+        <h3>Dados de Contato</h3>
+        <li><span><MapPin size={22} color='#FFEF37'/></span>R.Pereira Filgueiras, 1310 - Sala 103 - Aldeota</li>
+        <li><span><Phone size={22}color='#FFEF37'/></span>(99)-9999999</li>
+        <li><span><Envelope size={22}color='#FFEF37'/></span>e-mail</li>
+      </ul>
+
+      <ul className={styles.footerContactCalendary}>
         <h3>Horários</h3>
-        <ul>
-          <li><span><CalendarDots size={26}/></span>Segunda à Sexta-Feira das 09h00 às 17h00.</li>
-          <li><span><CalendarX size={26}/></span>Sábados,Domingos e Feriados:Fechado.</li>
-        </ul>
-      </div>
+        <li><span><CalendarDots size={22} color='#FFEF37'/></span>Segunda à Sexta-Feira das 09h00 às 17h00.</li>
+        <li><span><CalendarX size={22} color='#FFEF37'/></span>Sábados, Domingos e Feriados Fechado.</li>
+      </ul>
+
     </div>
   )
 }
